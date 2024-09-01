a60edc8626ced4ce0f1a95c614b9f6b09ff0c870
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-09-01 11:29:30 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -8,19 +8,40 @@ document.getElementById('add-task-btn').addEventListener('click', function() {
 
     const taskList = document.getElementById('task-list');
 
+    
     const newTask = document.createElement('li');
-    newTask.innerHTML = `${taskText} <button>Remove</button>`;
 
-    newTask.addEventListener('click', function() {
-        this.classList.toggle('completed');
+   
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('task-checkbox');
+
+    const taskLabel = document.createElement('span');
+    taskLabel.textContent = taskText;
+
+    
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+
+    
+    newTask.appendChild(checkbox);
+    newTask.appendChild(taskLabel);
+    newTask.appendChild(removeButton);
+
+    
+    checkbox.addEventListener('click', function() {
+        taskLabel.classList.toggle('completed');
     });
 
-    newTask.querySelector('button').addEventListener('click', function(e) {
+   
+    removeButton.addEventListener('click', function(e) {
         e.stopPropagation();
         this.parentElement.remove();
     });
 
+    
     taskList.appendChild(newTask);
 
+    
     document.getElementById('new-task').value = '';
 });
